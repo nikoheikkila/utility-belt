f3cb508e969cf4f342e122af3e9a28f9d1c46915
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-07-06 15:29:37 +0300

    fix: use Docker password directly

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker push "${IMAGE}"
