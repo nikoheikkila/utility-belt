dd12ea8cb7c88a01ae411392e96f6922fac86067
Niko Heikkilä <yo@nikoheikkila.fi>
2019-07-07 19:47:16 +0300

    feat(py): use textual output instead of JSON
    Script feels a lot more robust now.

--- scripts/link-check.py
+++ scripts/link-check.py
@@ -4,10 +4,12 @@ import json
 import click
 import meta
 
+from requests import Response
+from requests.exceptions import ConnectionError
 from requests_html import HTMLSession
-from typing import Dict
+from typing import Tuple
 
-LinkStatus = Dict[str, str]
+LinkStatus = Tuple[str, int]
 
 headers = {"User-Agent": meta.get_repo(), "X-Version": meta.get_version()}
 
@@ -26,7 +28,7 @@ def is_valid_url(link: str, pattern: str = "^https?://(.+)$") -> bool:
 
 def check_link_health(link: str) -> LinkStatus:
     """
-    Maps the given link to a dictionary containing the link and its 'health'.
+    Maps the given link to a tuple containing the link and its 'health'.
     This is executed using a HTTP/HEAD request following redirects.
 
     :param link:
@@ -34,7 +36,7 @@ def check_link_health(link: str) -> LinkStatus:
     """
     response = session.head(link, allow_redirects=True, headers=headers)
 
-    return {"url": link, "status": response.status_code}
+    return (link, response.status_code)
 
 
 @click.command()
@@ -44,25 +46,37 @@ def main(url: str, limit: int) -> int:
     """
     Checks a given URL for outbound links and their status.
     Good for detecting and fixing broken links on a web page.
+
+    May return false-positives since some websites may block incoming
+    HEAD requests.
     """
     if not is_valid_url(url):
         print(f"{url} is not a valid URL")
         return 1
 
-    links = session.get(url, headers=headers).html.links
+    try:
+        response = session.get(url, headers=headers)
+    except ConnectionError:
+        print(f"Unable to GET {url}")
+        return 1
+
+    links = response.html.links
 
     if not links:
         print(f"Source of {url} does not contain any links")
-        return 2
+        return 1
 
     outbound_links = [l for l in links if is_valid_url(l)]
 
     if len(outbound_links) > limit:
         outbound_links = outbound_links[:limit]
 
-    sanitized_links = [check_link_health(l) for l in outbound_links]
+    accepted = lambda status: status in range(200, 299)
 
-    print(json.dumps(list(sanitized_links)))
+    for l in outbound_links:
+        link, status = check_link_health(l)
+        mark = "✔️" if accepted(status) else "❌"
+        print(f"{link} {mark}  (status: {status})")
 
     return 0
 
